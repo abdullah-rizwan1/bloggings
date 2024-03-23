f4776b9882ca7bbec694c05f8aa9d47013993152
Abdullah Rizwan <abdullahrn8@gmail.com>
2024-03-23 16:27:30 +0500

    making use of route parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar'
 import Home from './Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateBlog from './CreateBlog'
+import BlogDetails from './BlogDetails'
 function App() {
   const title = 'Welcome to the New Blog'
   const likes = 50
@@ -12,7 +13,8 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" Component={Home} />
-            <Route path="/createBlog" Component={CreateBlog} />
+            <Route path="/create" Component={CreateBlog} />
+            <Route path="/blogs/:id" Component={BlogDetails} />
           </Routes>
         </div>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const BlogList = ({ blogs, title }) => {
   const allBlogs = blogs.map((blog) => {
     return (
       <div className="blog-preview" key={blog.id}>
-        <h1 className="blog-title">{blog.title}</h1>
-        <p className="blog-author">Author: {blog.author}</p>
+        <Link to={`/blogs/${blog.id}`}>
+          <h1 className="blog-title">{blog.title}</h1>
+          <p className="blog-author">Author: {blog.author}</p>
+        </Link>
       </div>
     )
   })
