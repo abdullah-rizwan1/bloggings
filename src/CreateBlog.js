5b3cd2feefd7913a69b28d6a8149009d19e02915
Abdullah Rizwan <abdullahrn8@gmail.com>
2024-03-25 06:47:10 +0500

    posting new blog to the json server

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -4,10 +4,28 @@ const CreateBlog = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('')
+  const [isPending, setIsPending] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = { title, body, author }
+
+    setIsPending(true)
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('New Blog Added')
+      setIsPending(false)
+    })
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title</label>
         <input
           type="text"
@@ -27,7 +45,8 @@ const CreateBlog = () => {
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog... </button>}
       </form>
     </div>
   )
