113e278518788e5ccf7269100c53527b443d84b7
Abdullah Rizwan <abdullahrn8@gmail.com>
2024-03-25 06:32:55 +0500

    added form controls to post a blog

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,7 +1,34 @@
+import { useState } from 'react'
+
 const CreateBlog = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('')
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}></textarea>
+        <label>Author</label>
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button>Add Blog</button>
+      </form>
     </div>
   )
 }
